c0991ebda9cbfc1d5ceb7f93331652383760ac89
leyla_qiao <leyla_qiao@163.com>
2024-12-21 14:17:35 +0800

    sync local note

--- .ci/sync-note.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NPM_PATH=/usr/local/bin/npm
-
-/usr/local/bin/npm run upload-note
-
-# Path: .ci/sync-note.sh
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # website-note
 
-- 同步最近文件至远端的两种方式
+- 同步最近文件至git的方式
 
 `npm run upload-note`
-
-`bash .ci/publish.sh`
